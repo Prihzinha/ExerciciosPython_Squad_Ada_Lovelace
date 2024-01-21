e79c2480f1f4dc9069e9c53a93318099a10363b6
Priscila Rodrigues de Aguiar <91269405+Prihzinha@users.noreply.github.com>
2024-01-21 15:15:30 -0300

    Delete 1_Exercícios_Conceitos_Básicos_de_Python_Ada_Lovelace/Desafio_2.py
    deletado, pois a lista completa está no arquivo Desafio1.py

--- 1_Exercícios_Conceitos_Básicos_de_Python_Ada_Lovelace/Desafio_2.py
+++
@@ -1,8 +0,0 @@
-from datetime import date, datetime
-data_nasc = input("infomme a data de nascimento (dd/mm/yyyy): ")
-##data_nasc = "05/05/1987"
-data_formatada = datetime.strptime(data_nasc, '%d/%m/%Y')
-data_atual = date.today()
-Idade_atual = data_atual.year - data_formatada.date().year - ((data_atual.month, data_atual.day) < (data_formatada.month, data_formatada.day))
-print(f"Data nascimento: {data_nasc} \nData agora: {date.strftime(data_atual,'%d/%m/%Y') } \nIdade: {Idade_atual}" )
-

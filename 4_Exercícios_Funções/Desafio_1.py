4763823935943a6dbfec39c9ad9ae38da4860780
Priscila Rodrigues de Aguiar <91269405+Prihzinha@users.noreply.github.com>
2024-01-24 12:20:05 -0300

    Update Desafio_1.py

--- 4_Exercícios_Funções/Desafio_1.py
+++ 4_Exercícios_Funções/Desafio_1.py
@@ -1,11 +1,12 @@
 #Faça um programa, com uma função que necessite de três
 #argumentos, e que forneça a soma desses três argumentos
 
-def somar(a, b):
-    resultado = a + b
+def somar(a, b, c):
+    resultado = a + b + c
     return resultado
 
 a = int(input("Informe um número: "))
 b = int(input("Informe outro número: "))
-print(f"A soma é: {somar(a,b)}")
+c = int(input("Informe o último número: "))
+print(f"A soma é: {somar(a,b,c)}")
     

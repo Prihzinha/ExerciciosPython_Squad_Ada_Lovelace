6f7279b9f771d8b26f0cb3879119e0334402ddaf
Priscila Rodrigues de Aguiar <91269405+Prihzinha@users.noreply.github.com>
2024-01-24 12:14:11 -0300

    Update Desafio_4.py

--- 3_Exercícios_Listas_Tuplas_e_Dicionários/Desafio_4.py
+++ 3_Exercícios_Listas_Tuplas_e_Dicionários/Desafio_4.py
@@ -1,11 +1,13 @@
 #Crie um dicionário representando contatos (nome, telefone).
 #Permita ao usuário procurar por um contato pelo nome.
 
-contato = {'Joao': "(99) 99999-9999", 'Maria': "(88) 88888.8888", 'Jose': "(77) 77777.7777", 'Cida': "(66) 66666.6666"}
-procurar = input ("Digite o nome: ")
-for chave, valor in contato.items():
-    if chave.upper() == procurar.upper():
-        print(f"{chave} localizado. Número Telefone {valor}")
-        break
+contato = {"Joao": "(99) 99999-9999", "Maria": "(88) 88888.8888", "Jose": "(77) 77777.7777", "Cida": "(66) 66666.6666"}
+
+def procurar_contato():
+    nome = input("Insira o nome do contato: ")
+    if nome in contato:
+        return contato[nome]
     else:
-        print(f"{procurar} não localizado")
+        return "Contato não encontrado."
+    
+print(procurar_contato())

6bd1ad56b92b3992148d0c867e73e017aa9cceff
Priscila Rodrigues de Aguiar <91269405+Prihzinha@users.noreply.github.com>
2024-01-24 10:49:33 -0300

    Update Desafio_6.py

--- 2_Exercícios_Tomada_de_decisão_e_Estruturas_de_Repetição/Desafio_6.py
+++ 2_Exercícios_Tomada_de_decisão_e_Estruturas_de_Repetição/Desafio_6.py
@@ -1,7 +1,11 @@
-login = input("Digite o login: ")
-senha = input("Digite a senha: ")
+print('Vamos ver como está sua memoria, preciso de seu login e senha para autenticação!')
 
-if login == "admin" and senha == "admin123":
-    print("Acesso permitido. Bem-vindo, admin!")
-else:
-    print("Erro: Login ou senha incorretos. Acesso negado!")
+While True:
+    login = input("Digite o login: ")
+    senha = input("Digite a senha: ")
+
+    if login == "admin" and senha == "admin123":
+       print("Acesso permitido. Bem-vindo, admin!")
+       break
+    else:
+        print("Erro: Login ou senha incorretos. Acesso negado. Tente novamente!")

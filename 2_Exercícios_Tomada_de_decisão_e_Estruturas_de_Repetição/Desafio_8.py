78a7fab9f99db58d8206e26916222f3ad940c251
Priscila Rodrigues de Aguiar <91269405+Prihzinha@users.noreply.github.com>
2024-01-24 10:55:01 -0300

    Update Desafio_8.py

--- 2_Exercícios_Tomada_de_decisão_e_Estruturas_de_Repetição/Desafio_8.py
+++ 2_Exercícios_Tomada_de_decisão_e_Estruturas_de_Repetição/Desafio_8.py
@@ -1,3 +1,5 @@
+print('Por gentileza utilize números inteiros!')
+
 numero1 = int(input("Digite o primeiro número: "))
 numero2 = int(input("Digite o segundo número: "))
 numero3 = int(input("Digite o terceiro número: "))

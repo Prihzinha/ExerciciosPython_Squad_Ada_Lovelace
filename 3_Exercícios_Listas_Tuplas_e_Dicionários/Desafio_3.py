f76fd63cbf2241a559d2e7f56c0eef0c212b5fda
Priscila Rodrigues de Aguiar <91269405+Prihzinha@users.noreply.github.com>
2024-01-24 19:58:08 -0300

    Update Desafio_3.py

--- 3_Exercícios_Listas_Tuplas_e_Dicionários/Desafio_3.py
+++ 3_Exercícios_Listas_Tuplas_e_Dicionários/Desafio_3.py
@@ -2,9 +2,34 @@
 #Adicione produtos (chaves) e quantidades (valores) ao carrinho.
 #Calcule o total do carrinho de compra.
 
-paises = {'Caneta': 1, 'Caderno': 10, 'Lapis': .5, 'Estojo': 15}
-for chave, valor in paises.items():
-    print(chave + " = R$ " + str(valor))
-    soma = float(valor)
+carrinho_de_compras = {}
 
-print(f"Valor Total= R$ {soma}")
+
+def adicionar_produto(carrinho, produto, quantidade, preco_unitario):
+    if produto in carrinho:
+        carrinho[produto]["quantidade"] += quantidade
+    else:
+        carrinho[produto] = {
+            "quantidade": quantidade,
+            "preco_unitario": preco_unitario}
+
+
+adicionar_produto(carrinho_de_compras, "Fone de Ouvido", 2, 50.0)
+adicionar_produto(carrinho_de_compras, "Carregador veícular", 1, 35.0)
+adicionar_produto(carrinho_de_compras, "Bateria recarregável", 3, 55.0)
+
+
+def calcular_total(carrinho):
+    total = 0
+    for produto, info in carrinho.items():
+        total += info["quantidade"] * info["preco_unitario"]
+    return total
+
+
+total_do_carrinho = calcular_total(carrinho_de_compras)
+
+print("Carrinho de Compras:")
+for produto, info in carrinho_de_compras.items():
+    print(f"{produto}: {info['quantidade']} x R${info['preco_unitario']:.2f}")
+
+print(f"\nTotal do Carrinho: R${total_do_carrinho:.2f}")

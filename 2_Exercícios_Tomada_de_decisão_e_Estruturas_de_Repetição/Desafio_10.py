d46a42565f3e8b51b0db014dc93ee9e87c06bfdf
Priscila Rodrigues de Aguiar <91269405+Prihzinha@users.noreply.github.com>
2024-01-24 10:16:08 -0300

    Update Desafio_10.py

--- 2_Exercícios_Tomada_de_decisão_e_Estruturas_de_Repetição/Desafio_10.py
+++ 2_Exercícios_Tomada_de_decisão_e_Estruturas_de_Repetição/Desafio_10.py
@@ -1,3 +1,5 @@
+print('Vamos ordenar números!')
+
 numero1 = int(input("Digite o primeiro número inteiro: "))
 numero2 = int(input("Digite o segundo número inteiro: "))
 numero3 = int(input("Digite o terceiro número inteiro: "))
